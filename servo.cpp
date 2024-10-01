845f84dd7dedcbfc1f84606a215d66b9c6a4e7bf
Snr1s3 <m1ss.sunrse@gmail.com>
2024-10-01 15:37:25 +0200

    update

---
+++ servo.cpp
@@ -0,0 +1,24 @@
+// C++ code
+#include <Servo.h>
+
+Servo servo;
+int p2 = 9;
+
+void setup()
+{
+  servo.attach(p2);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  for (int angle = 0; angle <= 180; angle++) {
+    servo.write(angle); 
+    delay(10); 
+  }
+
+  for (int angle = 180; angle >= 0; angle--) {
+    servo.write(angle); 
+    delay(10);
+  }
+}
